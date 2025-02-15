5a6de7c422cb196ef006a2966accc7a8771157a0
AyushTaparia <ayushtaparia1812@gmail.com>
2025-02-15 16:34:45 +0530

    fix: build error

--- src/providers/StoreProvider.tsx
+++ src/providers/StoreProvider.tsx
@@ -1,14 +1,14 @@
 'use client';
 
-import { useRef } from 'react';
-import { type StoreApi, type UseBoundStore } from 'zustand';
+// import { useRef } from 'react';
+// import { type StoreApi, type UseBoundStore } from 'zustand';
 
 export default function StoreProvider({ 
   children 
 }: { 
   children: React.ReactNode 
 }) {
-  const storeRef = useRef<null | UseBoundStore<StoreApi<any>>>(null);
+  // const storeRef = useRef<null | UseBoundStore<StoreApi<any>>>(null);
   
   return (
     <>
